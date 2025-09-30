13ed238430073c310a5263216fe22315a9e4a0b5
connorvokaty <connor.vokaty@ndsu.edu>
2025-09-30 12:09:51 -0500

    Actually starting the console commands to run the motion.

--- main.py
+++ main.py
@@ -1,36 +1,69 @@
+# Imports here
+
+
 # --------------------- ENTRY POINT TO THE PITCH MASTER SCRIPT ----------------------
 
-def say_hello(name, age):
-    print("Hello, " + name + "! Your age is " + age + ".")
+# Strickly using console commands
+print("Welcome to the Pitch-Master Control Panel!")
+print("1 = Sine")
+print("2 = Square")
+print("3 = Triangle")
+selected_waveform = input("Type the number corresponding to the waveform you desire: ")
 
-my_name = input("What is your name? ")
-my_age = input("How old are you? ")
+match selected_waveform:
+    case "1":
+        print("Sinusiodal Waveform selected!")
+    case "2":
+        print("Square Waveform selected!")
+    case "3":
+        print("Triangle Waveform selected!")
 
-say_hello(my_name, my_age)
+operating_speed = input("Type the operating speed you desire (Hz): ")
+ramp_time = input("Type the time required to achieve full speed (seconds): ")
 
-class Dog:
-    def __init__(self, name, breed, age):
-        self.name = name
-        self.breed = breed
-        self.age = age
 
-    def bark(self, is_loud):
-        if is_loud:
-            print("bark!!")
-        else:
-            print("woof!")
+#def help ():
+    #print("")
 
-    def get_older(self):
-        self.age += 1
 
-connors_dog = Dog("Freddy", "Golden Retriever", 4)
 
-print(connors_dog.age)
-connors_dog.bark(True)
-connors_dog.get_older()
 
-seths_dog = Dog("Seth", "Bobby", 8)
 
-print(seths_dog.breed)
-seths_dog.get_older()
-print(seths_dog.age)
+
+
+
+
+# def say_hello(name, age):
+#     print("Hello, " + name + "! Your age is " + age + ".")
+#
+# my_name = input("What is your name? ")
+# my_age = input("How old are you? ")
+#
+# say_hello(my_name, my_age)
+#
+# class Dog:
+#     def __init__(self, name, breed, age):
+#         self.name = name
+#         self.breed = breed
+#         self.age = age
+#
+#     def bark(self, is_loud):
+#         if is_loud:
+#             print("bark!!")
+#         else:
+#             print("woof!")
+#
+#     def get_older(self):
+#         self.age += 1
+#
+# connors_dog = Dog("Freddy", "Golden Retriever", 4)
+#
+# print(connors_dog.age)
+# connors_dog.bark(True)
+# connors_dog.get_older()
+#
+# seths_dog = Dog("Seth", "Bobby", 8)
+#
+# print(seths_dog.breed)
+# seths_dog.get_older()
+# print(seths_dog.age)
---
+++ motor control.py
@@ -0,0 +1,5 @@
+# Import libraries here
+
+# ----------------- HOLDS THE MOTOR CONTROL FUNCTIONS -----------------
+
+def start_sequence ():
